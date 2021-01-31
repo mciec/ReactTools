d487b76fe4bf32da4597c1317be2d714b9be9ad1
Michal Cieciora <mcieciora@descartes.com>
2021-01-31 14:19:06 +0100

    redux-thunk added

--- my-app/src/state/store.ts
+++ my-app/src/state/store.ts
@@ -1,6 +1,9 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension"
+import thunk from "redux-thunk"
 import { TextTransformer } from "./TextTransformer/types";
 import { TransformerReducer } from "./TextTransformer/reducer";
+
 
 const initialState: TextTransformer = {
   Src: { Text: "" },
@@ -8,9 +11,6 @@ const initialState: TextTransformer = {
   Transformations: [{ Prefix: "", Suffix: "" }],
 };
 
-const store = createStore<TextTransformer, any, any, any>(
-  TransformerReducer,
-  initialState
-);
+const store = createStore<TextTransformer, any, any, any>(TransformerReducer, initialState, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
