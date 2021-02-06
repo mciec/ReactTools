59046b9ee82cd85ad353f0362c054e1fd86080c8
mciec <mciec@o2.pl>
2021-02-06 12:18:45 +0100

    cleanup

--- my-app/src/components/TransformationItems/TransformationItems.tsx
+++ my-app/src/components/TransformationItems/TransformationItems.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from "react";
 import { Button } from "react-bootstrap";
-import { connect, useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import {
   AddTransformation,
   ExecTransformations,
@@ -12,69 +12,6 @@ import {
   Transformation,
 } from "../../state/TextTransformer/types";
 import { TransformationItem } from "../TransformationItem/TransformationItem";
-
-// type ObjectProps = {
-//   transformations: Transformation[];
-// };
-
-// type FunctionProps = {
-//   changeTransformationType: (i: number, t: Transformation) => any;
-//   addTransformation: (t: Transformation) => any;
-//   removeTransformation: (i: number) => any;
-// };
-
-// const transformationItems: FunctionComponent<ObjectProps & FunctionProps> = (
-//   props
-// ) => {
-//   return (
-//     <>
-//       {props.transformations.map((t, i) => {
-//         return (
-//           <TransformationItem
-//             transformation={t}
-//             changeTransformationType={(newTransformation: Transformation) => {
-//               props.changeTransformationType(i, newTransformation);
-//             }}
-//             removeTransformation={() => props.removeTransformation(i)}
-//             key={`transfItem${i}`}
-//           />
-//         );
-//       })}
-//       <Button
-//         variant="primary"
-//         size="lg"
-//         block
-//         onClick={(event: React.MouseEvent<HTMLElement, MouseEvent>) =>
-//           props.addTransformation({ Prefix: "", Suffix: "" })
-//         }
-//       >
-//         New transformation
-//       </Button>
-//     </>
-//   );
-// };
-
-// const mapStateToProps = function (state: TextTransformer): ObjectProps {
-//   return {
-//     transformations: state.Transformations,
-//   };
-// };
-
-// const mapDispatchToProps = function (dispatch: any): FunctionProps {
-//   return {
-//     changeTransformationType: (i: number, t: Transformation) =>
-//       dispatch(SetTransformation(i, t)),
-
-//     addTransformation: (t) => dispatch(AddTransformation(t)),
-//     removeTransformation: (i: number) => dispatch(RemoveTransformation(i)),
-//   };
-// };
-
-// export const TransformationItems = connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(transformationItems);
-
 export const TransformationItems: FunctionComponent = () => {
   const transformations: Transformation[] = useSelector<
     TextTransformer,
@@ -94,12 +31,14 @@ export const TransformationItems: FunctionComponent = () => {
 
   return (
     <>
-    <Button
-      variant="primary"
-      size="lg"
-      block
-      onClick={(event: React.MouseEvent<HTMLElement, MouseEvent>) => execTransformations()}
-    />
+      <Button
+        variant="primary"
+        size="lg"
+        block
+        onClick={(event: React.MouseEvent<HTMLElement, MouseEvent>) =>
+          execTransformations()
+        }
+      />
       {transformations.map((t, i) => {
         return (
           <TransformationItem
